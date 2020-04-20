102dd25ffaaeef0fc6f879f0e3942ae810ba8497
Ivan Koltcov <ikoltcov@yandex.ru>
2020-04-20 19:12:58 +0300

    master // Update biconvexLens

--- Scripts/ScriptedScene.py
+++ Scripts/ScriptedScene.py
@@ -1,6 +1,8 @@
-LoadScript("classes/BiconvexLens.py")
+LoadScript("classes/BiconvexLens.pi")
 
 scene = Scene()
+
 biconvex_lens_node = BiconvexLens()
 scene.AddNode(biconvex_lens_node)
+
 LoadScene(scene)
